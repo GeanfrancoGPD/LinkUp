1e438daa2f2b26b14c707c6df65bc726bda85bf9
luiscab19 <luis19alvarez05@gmail.com>
2026-08-08 23:35:38 -0400

    fix: uso de angular bien

---
+++ LinkUp/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.linkup.app',
+  appName: 'LinkUp',
+  webDir: 'dist/LinkUp/browser'
+};
+
+export default config;
